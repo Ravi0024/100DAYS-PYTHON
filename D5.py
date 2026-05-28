7a165941d39f221b384ff74d5c0ceb1e326b6360
RAVI <cheepururaviteja2005@gmail.com>
2026-05-28 22:15:39 +0530

    Add files via upload

---
+++ D5.py
@@ -0,0 +1,25 @@
+#day5 comments,escape sequence and print statement
+
+# to add or remove comments press (ctrl + /) in vs code
+
+
+'''A comment is a line of code that is not executed by the program. 
+   It is used to explain the code and make it easier to understand for other programmers who may read it in the future.'''
+
+print("hey i am  a good boy")
+
+
+"""escape sequence is a character that is used to represent a special character in a string.
+It is used to represent characters that are not allowed in a string, such as a newline or a tab.
+The most common escape sequences are:
+\n - represents a newline
+\t - represents a tab"""
+
+print("Ravi is a good boy\nHe is a good student")
+
+
+
+#separator is used to separate the values in a print statement.
+# by default the separator is a space, but you can change it to any character you want
+# print(object(s),sep=separator, end=end, file=file, flush=flush) 
+print("Ravi", "is", "a", "good", "boy", sep="-")    
