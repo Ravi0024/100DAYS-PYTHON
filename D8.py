8e75c280f2fdc10114a3c7fcf6c581386ad96dc0
RAVI <cheepururaviteja2005@gmail.com>
2026-05-31 19:33:51 +0530

    Add files via upload

---
+++ D8.py
@@ -0,0 +1,15 @@
+'''calculator solution of D7'''
+a=12
+b=2
+
+print("Addition of", a, "and", b, "is:", a + b) 
+
+print("Subtraction of", a, "and", b, "is:", a - b)  
+
+print("Multiplication of", a, "and", b, "is:", a * b)
+
+print("Division of", a, "and", b, "is:", a / b)
+
+print("Floor Division of", a, "and", b, "is:", a // b)
+
+    
