ea6158b8c801c610a5ee45eb4a0f9b0a1ed27d92
RAVI <cheepururaviteja2005@gmail.com>
2026-05-31 19:28:19 +0530

    Add files via upload

---
+++ D7.py
@@ -0,0 +1,16 @@
+'''CREATE A CALCULATOR PROGRAM CAPABLE OF PERFORMING ADDITION,SUBTRACTION,MULTIPLICATION,DIVISION OPERATIONS ON TWO NUMBERS.
+YOUR PROGRAME SHOULD FORMAT THE OUTPUT IN A READABLE MANNER.'''
+
+print("Welcome to the Calculator Program!")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+2
+print(num1 + num2)
+
+print(num1 - num2)
+
+print(num1 * num2)
+
+print(num1 / num2)
+
+print(num1//num2)
